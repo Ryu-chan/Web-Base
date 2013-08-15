f214e793534187beeea0512c44fa7849b1f09d4e
Hiro -san <Hiro@Hiro.(none)>
2013-08-14 23:13:07 -0400

    removed infrastructure again

--- _infrastructure/js/css/icons.css.js
+++
@@ -1,9 +0,0 @@
-$(function(){
-	//File Icons
-	$('a').each(function(){
-		var ico = $(this).attr('href').match(/\.[0-9a-z]+$/i)[0].substring(1)
-		$(this).css({
-			'background-image':'url(../res/icons/files/'+ico+'.png)'
-		})
-	})
-})
