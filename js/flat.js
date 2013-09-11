bf871ae0694a4ff8a204d42dfaf91504df29cc82
Chris <ckelley@7156-MBPro.local>
2013-09-10 23:10:51 -0400

    scss changes to flat

--- js/flat.js
+++ js/flat.js
@@ -5,9 +5,19 @@ $(function(){
 	$('[tooltip]').each(function(){
 		$(this).qtip({
 			content: $(this).attr('tooltip'),
-			show: 'mouseover',
-			hide: 'mouseout'
+			position: {
+				target: 'mouse',
+            	adjust: { x: 5, y: 5 },
+            	viewport: $(window)
+            }
 		})
+	})
+
+	//Accordion
+
+	/** Requires jquery, jqueryui */
+	$('[data-accordion]').each(function(){
+		$(this).accordion($.parseJSON($(this).data('accordion')));
 	})
 
 
